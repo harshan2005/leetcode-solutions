c4a581f1577d92380a3b1a620f078b4d92a038e5
HarshanKarthikeyan <151960128+harshan2005@users.noreply.github.com>
2025-08-04 16:47:30 +0530

    Time: 3 ms (45.79%) | Memory: 8.8 MB (25.85%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x<0||(x!=0&&x%10==0)){
-            return false;
-        }
-        int reversed=0;
-        while(x>reversed){
-            reversed=reversed*10+x%10;
-            x/=10;
-        }
-        return x==reversed||x==reversed/10;
+       if(x<0){
+        return false;
+    }
+    
+    int original=x;
+    long long reversed=0;
+    
+    while(x>0){
+        int digits=x%10;
+        reversed=reversed*10+digits;
+        x=x/10;
+    }
+    return original==reversed;
     }
 };
